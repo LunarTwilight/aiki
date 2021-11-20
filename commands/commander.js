df2f2d8b533aa7c78324850f7f23c0789ceaed21
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-11-20 04:54:03 -0900

    switch die command to commander command

---
+++ commands/commander.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const shell = require('shelljs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('commander')
+        .setDescription('Controls the bot')
+        .addSubCommand(subcommand =>
+            subcommand
+                .setName('update')
+                .setDescription('Updates the bot')
+        )
+        .addSubCommand(subcommand =>
+            subcommand
+                .setName('revert')
+                .setDescription('Reverts last update')
+        )
+        .addSubCommand(subcommand =>
+            subcommand
+                .setName('exec')
+                .setDescription('Executes a command')
+                .addStringOption(option =>
+                    option
+                        .setName('command')
+                        .setDescription('The command to run')
+                        .setRequired(true)
+                )
+        ),
+    async execute (interaction) {
+        await interaction.deferReply();
+        const command = interaction.options.getSubcommand() === 'exec' ? 'exec ' + interaction.options.getString('command') : interaction.options.getSubcommand()
+        shell.exec('pm2 deploy aiki ' + command, function (code, stdout, stderr) {
+            interaction.reply('Exit code: ', code);
+            interaction.followUp('Program output: ', stdout);
+            interaction.followUp('Program stderr: ', stderr);
+        });
+    }
+}
