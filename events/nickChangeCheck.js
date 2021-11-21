4005c4116650533a265a80937fb021b1844d2c56
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-11-21 03:22:31 -0900

    fix errors

--- events/nickChangeCheck.js
+++ events/nickChangeCheck.js
@@ -7,11 +7,8 @@ module.exports = {
         const diff = stringSimilarity.compareTwoStrings(oldUser.nickname, newUser.nickname);
         if (diff < 0.3) {
             newUser.guild.channels.cache.get(randomChannel).send('<@' + newUser.user.id + '> please keep your nick as your Fandom username. Your nick change has been reverted.');
-            newUser.member.setNickname({
-                nick: oldUser.nickname,
-                reason: 'Reverting nick change back to Fandom username'
-            });
+            newUser.setNickname(oldUser.nickname, 'Reverting nick change back to Fandom username');
         }
-        newUser.guild.channels.cache.get(renameLogChannel).send(`<@${newUser.user.id} changed their nick from ${oldUser.nickname} to ${newUser.nickname}\nSimilarity: ${diff}`);
+        newUser.guild.channels.cache.get(renameLogChannel).send(`<@${newUser.user.id}> changed their nick from \`${oldUser.nickname}\` to \`${newUser.nickname}\`\nSimilarity: ${diff}`);
     }
 }
