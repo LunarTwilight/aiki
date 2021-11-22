ae5fa68591148bb74b887c557f7c344b27db599e
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-11-22 04:16:07 -0900

    try to fix error & move these out of the function

--- expireMutes.js
+++ expireMutes.js
@@ -1,15 +1,15 @@
 const cron = require('node-cron');
 const { muteRole, modChanel, guildId } = require('./config.json');
 const db = require('./database.js');
+const getMutes = db.prepare('SELECT userid FROM mutes WHERE expiry < ?');
+const removeMuteRow = db.prepare('DELETE FROM mutes WHERE userid = ?');
 
 module.exports = {
     execute (client) {
-        const getMutes = db.prepare('SELECT userid FROM mutes WHERE expiry < ?');
-        const removeMuteRow = db.prepare('DELETE FROM mutes WHERE userid = ?');
         cron.schedule('0/15 * * * *', () => {
             const expiredMutes = getMutes.all(Date.now());
             expiredMutes.forEach(async row => {
-                client.guilds.cache.get(guildId).members.cache.get(row.userid).roles.remove(muteRole);
+                client.guilds.cache.get(guildId).members.cache.get(row.userid.toString()).roles.remove(muteRole);
                 client.channels.fetch(modChanel).send('<@' + row.userid + '> has been unmuted.');
                 removeMuteRow.run(row.userid);
             });
