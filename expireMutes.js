3de9d681fb0788058fcca9e1d8b56f42cf81745d
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-11-22 05:50:29 -0900

    sync WIP

--- expireMutes.js
+++ expireMutes.js
@@ -6,11 +6,14 @@ const removeMuteRow = db.prepare('DELETE FROM mutes WHERE userid = ?');
 
 module.exports = {
     execute (client) {
-        cron.schedule('0/15 * * * *', () => {
+        cron.schedule('* * * * *', () => { //0/15 * * * *
             const expiredMutes = getMutes.all(Date.now());
             expiredMutes.forEach(async row => {
-                client.guilds.cache.get(guildId).members.cache.get(row.userid.toString()).roles.remove(muteRole);
-                client.channels.fetch(modChanel).send('<@' + row.userid + '> has been unmuted.');
+                const roles = await client.guilds.cache.get(guildId).members.fetch(row.userid.toString()).roles;
+                if (roles.has(muteRole)) {
+                    roles.remove(muteRole);
+                }
+                await client.channels.fetch(modChanel).send('<@' + row.userid + '> has been unmuted.');
                 removeMuteRow.run(row.userid);
             });
         });
