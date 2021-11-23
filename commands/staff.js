d43c502c4caa4ec96b56aec728d7a8f1d2fd0231
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-11-22 15:45:35 -0900

    add staff command categories

--- commands/staff.js
+++ commands/staff.js
@@ -1,10 +1,36 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const baseUrl = 'https://support.fandom.com/hc/en-us/requests/new?ticket_form_id=';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('staff')
-        .setDescription('posts a link to zendesk'),
+        .setDescription('posts a link to zendesk')
+        .addStringOption(option =>
+            option.setName('category')
+                .setDescription('Optional category')
+                .addChoice('Account Help', 'account_help')
+                .addChoice('Wiki Changes', 'wiki_changes')
+                .addChoice('Problem', 'problem')
+                .addChoice('Protection', 'protection')
+        ),
     async execute (interaction) {
-        interaction.reply('Fandom: https://support.fandom.com\nGamepedia: https://support.gamepedia.com');
+        const cat = interaction.options.getString('category');
+        if (!cat) {
+            return interaction.reply('Fandom: https://support.fandom.com\nGamepedia: https://support.gamepedia.com');
+        }
+        switch (cat) {
+            case 'account_help':
+                interaction.reply(baseUrl + '360000931094');
+                break;
+            case 'wiki_changes':
+                interaction.reply(baseUrl + '360000931354');
+                break;
+            case 'problem':
+                interaction.reply(baseUrl + '360000940393');
+                break;
+            case 'protection':
+                interaction.reply(baseUrl + '360000948854');
+                break;
+        }
     }
 }
