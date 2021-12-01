22e12938ac1320e3c7631e0831508199579e983f
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-11-30 15:59:26 -0900

    handle user removing nick

--- events/nickChangeCheck.js
+++ events/nickChangeCheck.js
@@ -5,15 +5,16 @@ const config = db.prepare('SELECT renameLogChannel, randomChannel FROM config WH
 module.exports = {
     name: 'guildMemberUpdate',
     async execute (oldUser, newUser) {
-        if (!oldUser.nickname || !newUser.nickname || oldUser.nickname === newUser.nickname) {
+        if (!oldUser.nickname) {
             return;
         }
-        const diff = stringSimilarity.compareTwoStrings(oldUser.nickname, newUser.nickname);
+        const newName = newUser.nickname ? newUser.nickname : newUser.user.username;
+        const diff = stringSimilarity.compareTwoStrings(oldUser.nickname, newName);
         const { renameLogChannel, randomChannel } = config.all(BigInt(newUser.guild.id))[0];
         if (diff < 0.3) {
             newUser.guild.channels.cache.get(randomChannel).send('<@' + newUser.user.id + '> please keep your nick as your Fandom username. Your nick change has been reverted.');
             newUser.setNickname(oldUser.nickname, 'Reverting nick change back to Fandom username');
         }
-        newUser.guild.channels.cache.get(renameLogChannel).send(`<@${newUser.user.id}> changed their nick from \`${oldUser.nickname}\` to \`${newUser.nickname}\`\nSimilarity: ${diff}`);
+        newUser.guild.channels.cache.get(renameLogChannel).send(`<@${newUser.user.id}> ${newUser.nickname ? 'changed' : 'removed'} their nick.\nOld nick: \`${oldUser.nickname}\`\nNew ${newUser.nickname ? 'nick' : 'username'}:\`${newName}\`\nSimilarity: ${diff}`);
     }
 }
