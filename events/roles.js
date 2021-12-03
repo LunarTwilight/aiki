86ac9c2175e9815cf57f05631b0eaef874bdffb5
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-12-02 18:01:50 -0900

    format and reorder roles

--- events/roles.js
+++ events/roles.js
@@ -9,35 +9,39 @@ module.exports = {
 
         if (interaction.customId.match(/roles-addllanguages/)) {
             const rowLanguageA = new MessageActionRow().addComponents(
-                new MessageButton().setCustomId('roles-597697665618149377').setLabel('Korean').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-597697685134245888').setLabel('Indonesian').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-597697701186109450').setLabel('Dutch').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-597697722081869836').setLabel('Thai').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-597697739949867018').setLabel('Turkish').setStyle('SECONDARY')
-            ); const rowLanguageB = new MessageActionRow().addComponents(
-                new MessageButton().setCustomId('roles-597697765010833410').setLabel('Swedish').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-597697806982971428').setLabel('Finnish').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-597697828508401675').setLabel('Norwegian').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-598262772479819826').setLabel('Ukrainian').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043496950661161').setLabel('Czech').setStyle('SECONDARY')
-            ); const rowLanguageC = new MessageActionRow().addComponents(
-                new MessageButton().setCustomId('roles-826043562897965067').setLabel('Hungarian').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043650218655784').setLabel('Greek').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043729302257664').setLabel('Romanian').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043774005411880').setLabel('Malay').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043787364139021').setLabel('Arabic').setStyle('SECONDARY')
-            ); const rowLanguageD = new MessageActionRow().addComponents(
-                new MessageButton().setCustomId('roles-826043836232106014').setLabel('Hebrew').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043866544078918').setLabel('Danish').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043883279613982').setLabel('Estonian').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043893778219008').setLabel('Catalan').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826043905757151252').setLabel('Croatian').setStyle('SECONDARY')
-            ); const rowLanguageE = new MessageActionRow().addComponents(
-                new MessageButton().setCustomId('roles-826043945331064842').setLabel('Hindi').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043787364139021').setLabel('Arabic').setStyle('SECONDARY'),
                 new MessageButton().setCustomId('roles-826043961297731584').setLabel('Bulgarian').setStyle('SECONDARY'),
-                new MessageButton().setCustomId('roles-826044195985686569').setLabel('Tagalog').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043893778219008').setLabel('Catalan').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043905757151252').setLabel('Croatian').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043496950661161').setLabel('Czech').setStyle('SECONDARY')
+            );
+            const rowLanguageB = new MessageActionRow().addComponents(
+                new MessageButton().setCustomId('roles-826043866544078918').setLabel('Danish').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-597697701186109450').setLabel('Dutch').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043883279613982').setLabel('Estonian').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-597697806982971428').setLabel('Finnish').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043650218655784').setLabel('Greek').setStyle('SECONDARY')
+            );
+            const rowLanguageC = new MessageActionRow().addComponents(
+                new MessageButton().setCustomId('roles-826043836232106014').setLabel('Hebrew').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043945331064842').setLabel('Hindi').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043562897965067').setLabel('Hungarian').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-597697685134245888').setLabel('Indonesian').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-597697665618149377').setLabel('Korean').setStyle('SECONDARY')
+            );
+            const rowLanguageD = new MessageActionRow().addComponents(
+                new MessageButton().setCustomId('roles-826043774005411880').setLabel('Malay').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-597697828508401675').setLabel('Norwegian').setStyle('SECONDARY'),
                 new MessageButton().setCustomId('roles-826044236032901120').setLabel('Persian').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826043729302257664').setLabel('Romanian').setStyle('SECONDARY'),
                 new MessageButton().setCustomId('roles-826044250235076608').setLabel('Serbian').setStyle('SECONDARY')
+            );
+            const rowLanguageE = new MessageActionRow().addComponents(
+                new MessageButton().setCustomId('roles-597697765010833410').setLabel('Swedish').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-826044195985686569').setLabel('Tagalog').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-597697722081869836').setLabel('Thai').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-597697739949867018').setLabel('Turkish').setStyle('SECONDARY'),
+                new MessageButton().setCustomId('roles-598262772479819826').setLabel('Ukrainian').setStyle('SECONDARY')
             );
             return interaction.reply({
                 content: 'Select additional roles',
--- setupRoles.js
+++ setupRoles.js
@@ -66,6 +66,16 @@ module.exports = {
         const vertical = 'Available vertical roles:';
         const rowVertical = new MessageActionRow().addComponents(
             new MessageButton()
+                .setCustomId('roles-620721456745021441')
+                .setLabel('Anime')
+                .setStyle('SECONDARY')
+                .setEmoji('🍥'),
+            new MessageButton()
+                .setCustomId('roles-620723846818824192')
+                .setLabel('Books/Other')
+                .setStyle('SECONDARY')
+                .setEmoji('📚'),
+            new MessageButton()
                 .setCustomId('roles-620720867650830376')
                 .setLabel('Gaming')
                 .setStyle('SECONDARY')
@@ -74,52 +84,76 @@ module.exports = {
                 .setCustomId('roles-620720994822389806')
                 .setLabel('TV/Movies')
                 .setStyle('SECONDARY')
-                .setEmoji('📺'),
-            new MessageButton().setCustomId('roles-620721456745021441').setLabel('Anime').setStyle('SECONDARY').setEmoji('🍥'),
-            new MessageButton()
-                .setCustomId('roles-620723846818824192')
-                .setLabel('Books/Other')
-                .setStyle('SECONDARY')
-                .setEmoji('📚')
+                .setEmoji('📺')
         );
         const region = 'Select the region nearest your time zone:';
         const rowRegion = new MessageActionRow().addComponents(
             new MessageButton()
-                .setCustomId('roles-622890164351402005')
-                .setLabel('Europe/Africa')
+                .setCustomId('roles-622894005247803412')
+                .setLabel('Americas')
                 .setStyle('SECONDARY')
-                .setEmoji('🌍'),
+                .setEmoji('🌎'),
             new MessageButton()
                 .setCustomId('roles-622893997546930186')
                 .setLabel('Asia/Oceania')
                 .setStyle('SECONDARY')
                 .setEmoji('🌏'),
             new MessageButton()
-                .setCustomId('roles-622894005247803412')
-                .setLabel('Americas')
+                .setCustomId('roles-622890164351402005')
+                .setLabel('Europe/Africa')
                 .setStyle('SECONDARY')
-                .setEmoji('🌎')
+                .setEmoji('🌍')
         );
         const languageA = 'Available language roles:';
         const rowLanguageA = new MessageActionRow().addComponents(
-            new MessageButton().setCustomId('roles-597697552833576982').setLabel('Chinese').setStyle('SECONDARY'),
-            new MessageButton().setCustomId('roles-597697352698167306').setLabel('French').setStyle('SECONDARY'),
-            new MessageButton().setCustomId('roles-577582442278289414').setLabel('German').setStyle('SECONDARY'),
-            new MessageButton().setCustomId('roles-597697626741276681').setLabel('Italian').setStyle('SECONDARY'),
-            new MessageButton().setCustomId('roles-597697612069732413').setLabel('Japanese').setStyle('SECONDARY')
+            new MessageButton()
+                .setCustomId('roles-597697552833576982')
+                .setLabel('Chinese')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('roles-597697352698167306')
+                .setLabel('French')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('roles-577582442278289414')
+                .setLabel('German')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('roles-597697626741276681')
+                .setLabel('Italian')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('roles-597697612069732413')
+                .setLabel('Japanese')
+                .setStyle('SECONDARY')
         );
         const rowLanguageB = new MessageActionRow().addComponents(
-            new MessageButton().setCustomId('roles-597697583418179585').setLabel('Polish').setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('roles-597697583418179585')
+                .setLabel('Polish')
+                .setStyle('SECONDARY'),
             new MessageButton()
                 .setCustomId('roles-597697501684039681')
                 .setLabel('Portuguese/Brazilian')
                 .setStyle('SECONDARY'),
-            new MessageButton().setCustomId('roles-595523006034345985').setLabel('Russian').setStyle('SECONDARY'),
-            new MessageButton().setCustomId('roles-589138036298743818').setLabel('Spanish').setStyle('SECONDARY'),
-            new MessageButton().setCustomId('roles-597697646504837130').setLabel('Vietnamese').setStyle('SECONDARY')
+            new MessageButton()
+                .setCustomId('roles-595523006034345985')
+                .setLabel('Russian')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('roles-589138036298743818')
+                .setLabel('Spanish')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('roles-597697646504837130')
+                .setLabel('Vietnamese')
+                .setStyle('SECONDARY')
         );
         const rowLanguageC = new MessageActionRow().addComponents(
-            new MessageButton().setCustomId('roles-addllanguages').setLabel('Additional languages').setStyle('PRIMARY')
+            new MessageButton()
+                .setCustomId('roles-addllanguages')
+                .setLabel('Additional languages')
+                .setStyle('PRIMARY')
         );
         await channelWebhook.send({
             content: platform,
