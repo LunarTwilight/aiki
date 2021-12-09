a62a230e1231eb7930d69fa21513bb4bfeb78b3e
Melody <melody@janey.cf>
2021-12-09 18:16:49 +0000

    add custom response support

---
+++ commands/response.js
@@ -0,0 +1,114 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const db = require('../database.js');
+const config = db.prepare('SELECT modRole, verifiedRole FROM config WHERE guildId = ?');
+const getResponse = db.prepare('SELECT response FROM customResponses WHERE guildId = ? AND trigger = ?');
+const addResponse = db.prepare('INSERT INTO customResponses (trigger, response, guildId) VALUES (?, ?, ?)');
+const editResponse = db.prepare('UPDATE customResponses SET response = ? WHERE trigger = ? AND guildId = ?');
+const deleteResponse = db.prepare('DELETE FROM customResponses WHERE guildId = ? AND trigger = ?');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('response')
+        .setDescription('Control custom responses from the bot')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('print')
+                .setDescription('Prints out the current response to a specified trigger')
+                .addStringOption(trigger =>
+                    trigger
+                        .setName('name')
+                        .setDescription('The name of the trigger')
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('add')
+                .setDescription('Adds a new custom response to the bot')
+                .addStringOption(trigger =>
+                    trigger
+                        .setName('name')
+                        .setDescription('The name of the trigger')
+                        .setRequired(true)
+                )
+                .addStringOption(response =>
+                    response
+                        .setName('content')
+                        .setDescription('The message the bot will sent')
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('edit')
+                .setDescription('Edits a custom response')
+                .addStringOption(trigger =>
+                    trigger
+                        .setName('name')
+                        .setDescription('The name of the trigger')
+                        .setRequired(true)
+                )
+                .addStringOption(response =>
+                    response
+                        .setName('content')
+                        .setDescription('The message the bot will sent')
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('delete')
+                .setDescription('Deletes a custom response')
+                .addStringOption(trigger =>
+                    trigger
+                        .setName('name')
+                        .setDescription('The name of the trigger')
+                        .setRequired(true)
+                )
+        ),
+    async execute (interaction) {
+        const { modRole, verifiedRole } = config.all(interaction.guildId)[0];
+        switch (interaction.options.getSubcommand()) {
+            case 'print':
+                if (!interaction.members.roles.cache.has(verifiedRole.toString())) { 
+                    return interaction.reply({
+                        content: 'This command can not be used by verified users',
+                        ephemeral: true
+                    });
+                }
+                const { response } = getResponse.run(interaction.guildId, interaction.options.getString('name'));
+                interaction.reply('```\n' + response + '\n```');
+                break;
+            case 'add':
+                if (!interaction.members.roles.cache.has(modRole.toString())) {
+                    return interaction.reply({
+                        content: 'You are not a mod, I\'d suggest you become one.',
+                        ephemeral: true
+                    });
+                }
+                addResponse.run(interaction.options.getString('name'), interaction.options.getString('content'), interaction.guildId);
+                interaction.reply('Response added.');
+                break;
+            case 'edit':
+                if (!interaction.members.roles.cache.has(modRole.toString())) {
+                    return interaction.reply({
+                        content: 'You are not a mod, I\'d suggest you become one.',
+                        ephemeral: true
+                    });
+                }
+                editResponse.run(interaction.options.getString('content'), interaction.options.getString('name'), interaction.guildId);
+                interaction.reply('Reponse edited.');
+                break;
+            case 'delete':
+                if (!interaction.members.roles.cache.has(modRole.toString())) {
+                    return interaction.reply({
+                        content: 'You are not a mod, I\'d suggest you become one.',
+                        ephemeral: true
+                    });
+                }
+                deleteResponse.run(interaction.guildId, interaction.options.getString('name'));
+                interaction.reply('Response deleted.');
+                break;
+        }
+    }
+}
---
+++ events/customResponses.js
@@ -0,0 +1,20 @@
+const { whatEmoji } = require('../config.json');
+const db = require('../database.js');
+const config = db.prepare('SELECT verifiedRole FROM config WHERE guildId = ?');
+const getResponse = db.prepare('SELECT response FROM customResponses WHERE guildId = ? AND trigger = ?');
+
+module.exports = {
+    name: 'messageCreate',
+    execute (message) {
+        const { verifiedRole } = config.get(message.guild.id);
+        if (!message.content.startsWith('!') || !message.member.roles.cache.has(verifiedRole.toString())) {
+            return;
+        }
+        const row = getResponse.get(message.guild.id, message.content.slice(1));
+        if (!row) {
+            message.react(whatEmoji);
+            return;
+        }
+        message.channel.send(row.reponse);
+    }
+};
