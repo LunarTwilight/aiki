7e53d6837c4a681ac8c2b475d5224f1a1f2f3eab
Melody <melody@janey.cf>
2021-12-11 09:44:13 +0000

    exclude more prefixes

--- events/customResponses.js
+++ events/customResponses.js
@@ -2,12 +2,13 @@ const { whatEmoji } = require('../config.json');
 const db = require('../database.js');
 const config = db.prepare('SELECT verifiedRole FROM config WHERE guildId = ?');
 const getResponse = db.prepare('SELECT response FROM customResponses WHERE guildId = ? AND trigger = ?');
+const excluded = ['!wiki', '!report', '!soap'];
 
 module.exports = {
     name: 'messageCreate',
     execute (message) {
         const { verifiedRole } = config.get(message.guild.id);
-        if (!message.content.startsWith('!') || message.content.startsWith('!wiki') || !message.member.roles.cache.has(verifiedRole.toString())) {
+        if (!message.content.startsWith('!') || excluded.some(prefix => message.content.startsWith(prefix)) || !message.member.roles.cache.has(verifiedRole.toString())) {
             return;
         }
         const row = getResponse.get(message.guild.id, message.content.slice(1));
