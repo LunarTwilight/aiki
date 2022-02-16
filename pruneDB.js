d6e828eafbb2604d6d7647b1e6da6488fe24d260
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2022-02-16 09:00:38 -0900

    update to do rename notice ignores instead

--- pruneDB.js
+++ pruneDB.js
@@ -1,25 +1,13 @@
 const cron = require('node-cron');
 const db = require('./database.js');
-const config = db.prepare('SELECT verifiedRole FROM config WHERE guildId = ?');
-const getIgnoredUsers = db.prepare('SELECT userId, guildId FROM verificationIgnore');
-const removeIgnoreUserRow = db.prepare('DELETE FROM verificationIgnore WHERE userId = ?');
+const getIgnoredUsers = db.prepare('SELECT userId FROM renameNoticeIgnore WHERE expiry > ?');
+const removeIgnoreUserRow = db.prepare('DELETE FROM renameNoticeIgnore WHERE userId = ?');
 
 module.exports = {
     execute (client) {
         cron.schedule('0/30 * * * *', () => {
-            const ignoredUsers = getIgnoredUsers.all();
-            ignoredUsers.forEach(async row => {
-                const guild = await client.guilds.fetch(row.guildId);
-                let user;
-                try {
-                    user = await guild.members.fetch(row.userId);
-                } catch (error) {
-                    //fall through
-                }
-                const role = config.get(row.guildId).verifiedRole;
-                if (!user || user.roles.cache.has(role)) {
-                    removeIgnoreUserRow.run(row.userId);
-                }
+            getIgnoredUsers.all(Date.now()).forEach(row => {
+                removeIgnoreUserRow.run(row.userId);
             });
         });
     }
