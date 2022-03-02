05e6db5afcbf8cff33bf0e9b3d3ae31b23167692
Melody <melody@janey.cf>
2022-03-02 07:56:45 +0000

    handle changing nicks differently

---
+++ commands/nick.js
@@ -0,0 +1,87 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const stringSimilarity = require('string-similarity');
+const db = require('../database.js');
+const config = db.prepare('SELECT renameLogChannel FROM config WHERE guildId = ?');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('nick')
+        .setDescription('Changes your nick (don\'t set a new nick to remove your nick)')
+        .addStringOption(option =>
+            option
+                .setName('nick')
+                .setDescription('Your new nick')
+        ),
+    async execute (interaction) {
+        if (!interaction.inGuild()) {
+            return await interaction.reply({
+                content: 'This command is only avalible in a server.',
+                ephemeral: true
+            });
+        }
+        if (!interaction.member.manageable) {
+            return await interaction.reply({
+                content: 'Bot is unable to change your nick because you\'re higher than it, please use Discord\'s native nick change feature.',
+                ephemeral: true
+            });
+        }
+        if (interaction.member.nickname === interaction.options.getString('nick')) {
+            return await interaction.reply({
+                content: 'Your new nick is the same as your old nick.',
+                ephemeral: true
+            });
+        }
+        if (!interaction.member.nickname && !interaction.options.getString('nick')) {
+            return await interaction.reply({
+                content: 'You can not remove a nick you have not set.',
+                ephemeral: true
+            });
+        }
+        const newName = interaction.options.getString('nick') || interaction.member.user.username;
+        const diff = stringSimilarity.compareTwoStrings(interaction.member.displayName.toLowerCase(), newName.toLowerCase());
+        const embed = new MessageEmbed()
+            .addFields({
+                name: 'User',
+                value: '<@' + interaction.member.id + '>',
+                inline: true
+            }, {
+                name: `Old ${interaction.member.nickname ? 'nick' : 'username'}`,
+                value: interaction.member.displayName,
+                inline: true
+            }, {
+                name: `New ${interaction.options.getString('nick') ? 'nick': 'username'}`,
+                value: newName,
+                inline: true
+            }, {
+                name: 'Similarity',
+                value: diff,
+                inline: true
+            });
+        if (!interaction.options.getString('nick')) {
+            embed.setTitle('User removed nick');
+        } else if (!interaction.member.nickname) {
+            embed.setTitle('User set nick');
+        } else {
+            embed.setTitle('User changed nick');
+        }
+        const { renameLogChannel } = config.get(interaction.guildId);
+        await newUser.client.channels.cache.get(renameLogChannel).send({
+            embeds: [
+                embed
+            ]
+        });
+        if (diff < 0.3) {
+            await interaction.reply({
+                content: 'New name is not similar to your Fandom username, and has therefor not been changed. Please re-verify yourself in <#928414471469277194> if your Fandom username has changed.',
+                ephemeral: true
+            });
+        } else {
+            await interaction.member.setNickname(newName, 'Requested by user');
+            await interaction.reply({
+                content: 'Your nick has been changed.',
+                ephemeral: true
+            });
+        }
+    }
+};
