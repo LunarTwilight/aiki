8061769787f8dac2c8bb07d12296a912a0a1386d
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2022-04-09 16:00:58 +0800

    Update blockedResponse.js

--- events/blockedResponse.js
+++ events/blockedResponse.js
@@ -28,6 +28,8 @@ module.exports = {
                     components: [button],
                     ephemeral: true
                 });
+                
+                break;
 
             case 'blocked-local':
                 let localBlockedMessage = `If you have been blocked on one community, and you do not know why, or you disagree with the reason, __stay calm__. Please do the following steps to appeal your block:
@@ -35,9 +37,13 @@ module.exports = {
 2. If they disallowed editing your talk or wall, try leaving them a message on [Community Central](https://c.fandom.com).
 3. If they still won't appeal your block, you can simply wait it out (if it's not indefinite), or join another Fandom community.
 
-**Note**: If they are abusing their rights (like blocking anyone who wants to edit), please contact another admin about the issue. **If there are no other admins, please [contact Fandom staff](https://support.fandom.com/hc/en-us/requests/new?ticket_form_id=360000931094&tf_360017834073=__dc.my_account_is_blocked__&tf_1500002786382=fandom_block_local) asap**.`;
+**Note**: If they are abusing their rights (like blocking anyone who wants to edit), please contact another admin about the issue.
+**If there are no other admins, please [contact Fandom staff](https://support.fandom.com/hc/en-us/requests/new?ticket_form_id=360000931094&tf_360017834073=__dc.my_account_is_blocked__&tf_1500002786382=fandom_block_local) asap**.`;
 
                 interaction.reply({ content: localBlockedMessage, ephemeral: true });
+            
+            default:
+                interaction.reply({ content: 'Error: no subcommand specified!', ephemeral: true })
         }
     },
 };
