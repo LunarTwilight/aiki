a90a607f1c2a5ba0d1426bf551b94ffb2c5c3ea6
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2022-04-09 02:55:56 -0800

    fix errors/warnings from linter and apply some formatting changes

--- commands/blocked.js
+++ commands/blocked.js
@@ -5,19 +5,18 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('blocked')
         .setDescription('Gives information on what to do when you (or another user) is blocked'),
-
     async execute (interaction) {
         const button = new MessageActionRow().addComponents(
             new MessageButton()
                 .setCustomId('blocked-global')
-                .setLabel("I'm globally blocked")
+                .setLabel('I\'m globally blocked')
                 .setStyle('PRIMARY'),
 
             new MessageButton()
                 .setCustomId('blocked-local')
-                .setLabel("I'm locally blocked")
+                .setLabel('I\'m locally blocked')
                 .setStyle('PRIMARY'),
-            
+
             new MessageButton()
                 .setURL('https://c.fandom.com/wiki/Help:I_have_been_blocked')
                 .setLabel('General help page')
@@ -26,8 +25,8 @@ module.exports = {
 
         await interaction.reply({
             content:
-                "Blocked? Don't panic! See the linked pages for information on what to do. \n\nNote: **Do __not__ create a seperate account**, that is considered [sockpuppetry](https://c.fandom.com/Help:Sockpuppetry). Instead, follow the instructions in the linked help page.",
+                'Blocked? Don\'t panic! See the linked pages for information on what to do. \n\nNote: **Do __not__ create a seperate account**, that is considered [sockpuppetry](https://c.fandom.com/Help:Sockpuppetry). Instead, follow the instructions in the linked help page.',
             components: [button]
         });
-    },
+    }
 };
