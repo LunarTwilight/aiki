14deadf97b4e224958328b02b2b1914ebdff25f0
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2022-07-26 06:41:31 -0800

    use full path for DB

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const Database = require('better-sqlite3');
-const db = new Database('db.sqlite', {
+const db = new Database('/root/aiki/db.sqlite', {
     fileMustExist: true
 });
 db.defaultSafeIntegers(true);
