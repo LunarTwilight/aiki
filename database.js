89f2e9caf8c947999ed3d56b81263bb63fd510c7
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2022-09-24 19:29:03 -0800

    DB doesn't contain any bitints so I don't think we need this anymore?

--- database.js
+++ database.js
@@ -2,5 +2,5 @@ const Database = require('better-sqlite3');
 const db = new Database(__dirname + '/db.sqlite', {
     fileMustExist: true
 });
-db.defaultSafeIntegers(true);
+//db.defaultSafeIntegers(true);
 module.exports = db;
