b7ae94ee1d9fe0d9106cb1fa5e07d003d5c1e2b7
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2022-09-28 07:04:22 -0800

    alright lets bring this back (kinda)

--- keepChannelOrder.js
+++ keepChannelOrder.js
@@ -1,14 +1,17 @@
 const db = require('./database.js');
 const cron = require('node-cron');
 const channelPositions = db.prepare('SELECT channelId, position FROM channelPositions WHERE guildId = ?');
+const { testServer } = require('./config.json');
 
 module.exports = {
     name: 'ready',
     once: true,
     async execute (client) {
-        return;
-        cron.schedule('0 * * * *', () => {
+        //cron.schedule('0 * * * *', () => {
             client.guilds.cache.each(guild => {
+                if (testServer !== guild.id) {
+                    return;
+                }
                 //https://stackoverflow.com/a/50951372
                 const positions = channelPositions.all(guild.id).map(({
                     channelId,
@@ -24,6 +27,6 @@ module.exports = {
                     })
                     .catch(console.error);
             });
-        });
+        //});
     }
 };
