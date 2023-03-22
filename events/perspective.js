0dbaaa0d00737b5933e4cc9d535d1f4b95c5d378
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2023-03-22 12:22:58 -0800

    bump threshold up to 0.8

--- events/perspective.js
+++ events/perspective.js
@@ -12,7 +12,7 @@ const config = db.prepare('SELECT modChannel, modRole FROM config WHERE guildId 
 const formatScores = async scores => {
     const list = [];
     _.each(scores, (value, key) => {
-        if (value >= 0.7) {
+        if (value >= 0.8) {
             list.push(key);
         }
     });
@@ -74,7 +74,7 @@ module.exports = {
         });
         const scores = await calculateScores(result);
 
-        if (_.some(Object.values(scores), item => item >= 0.7)) {
+        if (_.some(Object.values(scores), item => item >= 0.8)) {
             const embed = new EmbedBuilder()
                 .setTitle('Possible mod action needed')
                 .setURL(message.url)
