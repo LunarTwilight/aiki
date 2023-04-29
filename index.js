14f176b73cb0edbaa41ce57c11ad2fe196807802
Melody <melody@janey.cf>
2023-04-28 22:25:27 -0400

    not gonna do this at this time

--- index.js
+++ index.js
@@ -45,8 +45,6 @@ http.createServer(async (req, res) => {
     }
 }).listen(22022);
 
-require('./keepChannelOrder.js').execute(client);
-
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
