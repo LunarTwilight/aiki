7dd97e6f8705f651cb256a8a783323c1daf8b876
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2023-11-21 01:54:17 -0900

    whoops forgot about this

--- events/automodThreadTitles.js
+++ events/automodThreadTitles.js
@@ -10,7 +10,7 @@ module.exports = {
         }
         const { modLogChannel, modChannel, modRole } = config.all(thread.guild.id)[0];
         const owner = await thread.guild.members.cache.get(thread.authorId);
-        if (owner.user.bot || owner.guildMember.roles.highest.comparePositionTo(modRole) >= 0) {
+        if (owner.user.bot || owner.roles.highest.comparePositionTo(modRole) >= 0) {
             return;
         }
 
