e3defda564b287c25fa2ec293655af8ac64273ed
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2023-11-21 02:18:55 -0900

    got it working

--- events/automodThreadTitles.js
+++ events/automodThreadTitles.js
@@ -10,7 +10,6 @@ module.exports = {
         }
         const { modLogChannel, modChannel, modRole } = config.all(thread.guild.id)[0];
         const owner = await thread.guild.members.fetch(thread.ownerId);
-        console.log(thread, owner);
         if (owner.user.bot || owner.roles.highest.comparePositionTo(modRole) >= 0) {
             return;
         }
