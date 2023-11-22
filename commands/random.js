ede3f1b6d75b15ce16b1b25e65e423cf8d3613d6
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2023-11-21 22:14:01 -0900

    try this out

--- commands/random.js
+++ commands/random.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const db = require('../database.js');
 const config = db.prepare('SELECT modRole, randomChannel FROM config WHERE guildId = ?');
 
@@ -6,16 +6,10 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('random')
         .setDescription('Tells users to go to random')
-        .setDMPermission(false),
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
     async execute (interaction) {
-        const { modRole, randomChannel } = config.all(interaction.guildId)[0];
-        if (interaction.member.roles.highest.comparePositionTo(modRole) < 0) {
-            await interaction.reply({
-                content: 'You are not a mod, I\'d suggest you become one.',
-                ephemeral: true
-            });
-            return;
-        }
+        const { randomChannel } = config.all(interaction.guildId)[0];
         await interaction.reply(`The mods request that you move this convo to <#${randomChannel}>.`);
     }
 };
