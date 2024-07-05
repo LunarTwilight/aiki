d1eafc84742846bc7a4d0ede556b912645ff2ad8
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2024-07-05 13:41:50 -0800

    try breaking the file up into two handlers, rather than trying to handle it all on one interaction

--- events/reports.js
+++ events/reports.js
@@ -6,7 +6,6 @@ module.exports = {
     name: 'interactionCreate',
     async execute (interaction) {
         if (interaction.isButton() && interaction.customId === 'create-private-report-thread') {
-            const { modChannel } = config.all(interaction.guild.id)[0];
             const modal = new ModalBuilder()
                 .setCustomId(`create-private-report-thread`)
                 .setTitle(`Please enter the title of your report`);
@@ -21,33 +20,32 @@ module.exports = {
             modal.addComponents(row);
 
             await interaction.showModal(modal);
+        }
 
+        if (interaction.isModalSubmit() && interaction.customId === 'create-private-report-thread') {
+            const { modChannel } = config.all(interaction.guild.id)[0];
             try {
-                const modalInteraction = await interaction.awaitModalSubmit({
-                    time: 180000, //3 min
-                    filter: i => i.user.id === interaction.user.id
-                });
-                console.log('modal submit event', modalInteraction);
+                console.log('modal submit event', interaction);
                 const thread = await interaction.channel.threads.create({
                     type: ChannelType.PrivateThread,
-                    name: modalInteraction.fields.getTextInputValue('title').trim(),
+                    name: interaction.fields.getTextInputValue('title').trim(),
                     reason: `Private report thread created upon request of ${(interaction.member.nickname || interaction.user.displayName)}`
                 });
                 console.log('thread', thread);
                 await thread.members.add(interaction.user.id);
                 await thread.send(`Hey <@${interaction.user.id}>! Please reply in this thread and provide more information about your report, the moderators will look into it as soon as possible!`);
-                await modalInteraction.reply({
+                await interaction.reply({
                     content: `Your report has been created at <#${thread.id}>. Please fill out the report by sending messages in the thread.`,
                     ephemeral: true
                 });
                 const alertEmbed = new EmbedBuilder()
                     .setTitle('New private report created!')
-                    //.setURL(thread.url)
+                    .setURL(thread.url)
                     .setAuthor({
                         name: (interaction.member.nickname || interaction.user.displayName),
                         iconURL: (interaction.member.displayAvatarURL())
                     })
-                    .setDescription(modalInteraction.fields.getTextInputValue('title').trim() + '\n\n' + thread.url)
+                    .setDescription(interaction.fields.getTextInputValue('title').trim())
                     .setTimestamp();
                 console.log('alert embed', alertEmbed);
                 await interaction.guild.channels.fetch(modChannel).then(async channel => {
@@ -58,7 +56,7 @@ module.exports = {
             } catch (error) {
                 console.error('report error', error);
                 await interaction.followUp({
-                    content: 'The modal was not submitted in time, or another error occured.',
+                    content: 'An error occured while handing your submission, please ping a moderator.',
                     ephemeral: true
                 });
             }
