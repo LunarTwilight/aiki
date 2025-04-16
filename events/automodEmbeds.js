3fecf19cbdb7b6d9940ae5a608ec957c51114a93
arzehero <167118469+arzehero@users.noreply.github.com>
2025-04-16 15:37:21 -0600

    feat: supress embeds in specific categories unless they point to community central

---
+++ events/automodEmbeds.js
@@ -0,0 +1,34 @@
+const db = require('../database.js');
+const config = db.prepare('SELECT modLogChannel, modChannel, messageLogChannel, modRole FROM config WHERE guildId = ?');
+
+module.exports = {
+    name: 'messageCreate',
+    async execute (message) {
+        const { modRole } = config.all(message.guild.id)[0];
+        if (message.author.bot || message.member.roles.highest.comparePositionTo(modRole) >= 0) {
+            return;
+        }
+
+		const watchedCategories = [
+			'563020189604773890', // general
+			'575022973568811019', // technical
+		];
+
+		const category = message.channel.isThread() ? message.channel.parent?.parentId : message.channel.parentId;
+		if (!watchedCategories.includes(category)) {
+            return;
+        }
+
+		const fandomEmbeds = message.embeds.some(embed => {
+			if (embed.data.type !== 'link') {
+				return false;
+			}
+
+			const host = new URL(embed.data.url).host
+			return host.endsWith('.fandom.com') && host !== 'community.fandom.com'
+		})
+		if (fandomEmbeds) {
+			await message.suppressEmbeds();
+		}
+    }
+};
