de589064050d69f0dee1d93699cefac90412d6b3
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2026-06-17 19:40:40 -0800

    clean up check with help from gemini

--- extensions.js
+++ extensions.js
@@ -4,14 +4,11 @@ const getConfig = require('./config.js');
 function initExtensions () {
     Object.defineProperty(GuildMember.prototype, 'isMod', {
         get () {
-            if (this.permissions.has(PermissionFlagsBits.Administrator)) {
-                return true;
-            }
-            if (this.permissions.has(PermissionFlagsBits.ModerateMembers)) {
-                return true;
-            }
             const { modRole } = getConfig(this.guild.id);
-            return (this.roles.cache.has(modRole));
+            return (this.permissions.has([
+                PermissionFlagsBits.Administrator,
+                PermissionFlagsBits.ModerateMembers
+            ], false) || this.roles.cache.has(modRole));
         },
         configurable: true,
         enumerable: true
